be196b076504b95f0666964b0eca16ffb4dfa8d0
Brad <brad@4pi.com.au>
2024-06-25 08:22:42 +1000

    Added 1Password CLI installed check

--- src/OPSSHAssistant/Program.cs
+++ src/OPSSHAssistant/Program.cs
@@ -1,12 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Diagnostics;
-using System.Text.Json;
 using OPSSHAssistant.Data;
 using CliWrap;
-using CliWrap.Buffered;
 using OPSSHAssistant;
 using Spectre.Console;
+
+try
+{
+    var result = await Cli.Wrap("op")
+        .WithArguments("--help") // --version does not return a valid exit code, so using --help instead.
+        .ExecuteAsync();
+}
+catch (Exception err)
+{
+    Debug.WriteLine($"Error: {err.Message}");
+    Console.WriteLine("1Password CLI could not be found. Please install it from here, https://developer.1password.com/docs/cli/get-started/");
+    Debugger.Break();
+    Environment.Exit(0);
+}
+
+
+
 
 var prompt = new SelectionPrompt<string>()
     .Title("This tool will use the 1Password CLI to list accounts, vaults, and items. Are you sure you want to continue?")
